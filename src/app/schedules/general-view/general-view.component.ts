bf615b5fb2123ed11c944d95c8f8b8abd35c48f5
Darío Germán Uberti Manassero <dariogum@gmail.com>
2018-11-13 13:11:53 -0300

    working on schedules

--- src/app/schedules/general-view/general-view.component.ts
+++ src/app/schedules/general-view/general-view.component.ts
@@ -11,19 +11,75 @@ import * as moment from 'moment';
 })
 export class GeneralViewComponent implements OnInit {
 
+  SCHEDULES = [
+    {
+      id: 1,
+      name: 'Agenda 1',
+      professional: 1,
+      weekDays: [0, 1, 2, 3, 4, 5, 6],
+      periodicity: ['weekly'], //['monthly']
+      validity: {
+        start: moment('20180101'),
+        end: moment('20181231'),
+      },
+      interval: 20,
+      color: '#bbdefb',
+    },
+    {
+      id: 2,
+      name: 'Agenda 2',
+      professional: 2,
+      weekDays: [0, 1, 2, 3, 4, 5, 6],
+      periodicity: ['weekly'], //['monthly']
+      validity: {
+        start: moment('20180101'),
+        end: moment('20181231'),
+      },
+      interval: 30,
+      color: '#dcedc8',
+    },
+  ];
+
   APPOINTMENTS = [
     {
-      date: moment().format('DD/MM/YYYY'),
+      schedule: this.SCHEDULES[1],
+      id: 1,
+      date: moment(),
+      hour: moment().format('HH:mm'),
+      patient: 'Lola'
+    },
+    {
+      schedule: this.SCHEDULES[0],
+      id: 2,
+      date: moment(),
+      hour: moment().format('HH:mm'),
+      patient: 'Tini'
+    },
+    {
+      schedule: this.SCHEDULES[1],
+      id: 3,
+      date: moment(),
       hour: moment().format('HH:mm'),
       patient: 'Pepe'
     },
     {
-      date: moment().format('DD/MM/YYYY'),
+      schedule: this.SCHEDULES[1],
+      id: 1,
+      date: moment('20181110'),
       hour: moment().format('HH:mm'),
-      patient: 'Pepe'
+      patient: 'Lola'
     },
     {
-      date: moment().format('DD/MM/YYYY'),
+      schedule: this.SCHEDULES[0],
+      id: 2,
+      date: moment('20181110'),
+      hour: moment().format('HH:mm'),
+      patient: 'Tini'
+    },
+    {
+      schedule: this.SCHEDULES[1],
+      id: 3,
+      date: moment('20181120'),
       hour: moment().format('HH:mm'),
       patient: 'Pepe'
     },
@@ -31,27 +87,20 @@ export class GeneralViewComponent implements OnInit {
 
   currentUser = JSON.parse(localStorage.getItem('currentUser')).id;
   screenType = 'wide';
-  schedules = [
+  todayName = moment().format('dddd DD/MM/YYYY');
+  today = moment();
+  userRole: string;
+  validSchedules = [];
+  viewType = 'daily';
+  selectedSchedules = [];
+
+  lists = [
     {
-      id: 1,
-      name: "Agenda 1"
-    },
-    {
-      id: 2,
-      name: "Agenda 2"
-    },
-  ];
-  today = moment().format('dddd DD/MM/YYYY');
-  todayDate = moment().format('DD/MM/YYYY');
-  tiles = [
-    {
-      name: this.today,
-      date: this.todayDate,
+      name: this.todayName,
+      date: this.today,
       appointments: [],
     }
   ];
-  userRole: string;
-  viewType = 'daily';
 
   constructor(
     private breakpointObserver: BreakpointObserver,
@@ -87,27 +136,32 @@ export class GeneralViewComponent implements OnInit {
         this.screenType = 'wide';
       }
     });
+
+    moment.locale('es');
+
+    this.getValidSchedules();
   }
 
   changeTypeOfView(event) {
     switch (event.value) {
       case "weekly":
-        this.tiles = this.getDays('isoWeek');
+        this.lists = this.getDays('isoWeek');
         this.viewType = event.value;
         break;
       case "monthly":
-        this.tiles = this.getDays('month');
+        this.lists = this.getDays('month');
         this.viewType = event.value;
         break;
       default:
-        this.tiles = [{
-          name: this.today,
-          date: this.todayDate,
+        this.lists = [{
+          name: this.todayName,
+          date: this.today,
           appointments: [],
         }];
         this.viewType = event.value;
         break;
     }
+    this.getValidSchedules();
   }
 
   getDays(period) {
@@ -117,9 +171,9 @@ export class GeneralViewComponent implements OnInit {
     let day = startOfWeek;
     while (day <= endOfWeek) {
       days.push({
-          name: day.format('dddd DD/MM/YYYY'),
-          date: day.format('DD/MM/YYYY'),
-          appointments: [],
+        name: day.format('dddd DD/MM/YYYY'),
+        date: day,
+        appointments: [],
       });
       day = day.clone().add(1, 'd');
     }
@@ -129,18 +183,57 @@ export class GeneralViewComponent implements OnInit {
   changeTypeOfAppointments(event) {
     switch (event.value) {
       case "available":
-        
+
         break;
       case "free":
-        
+
         break;
       case "programmed":
-        
+
         break;
       default:
-        
+
         break;
     }
+  }
+
+  getValidSchedules() {
+    this.validSchedules = [];
+    switch (this.viewType) {
+      case "weekly":
+        for (let i = this.SCHEDULES.length - 1; i >= 0; i--) {
+          if (moment(this.SCHEDULES[i].validity.start) <= moment().startOf('isoWeek') &&
+            moment(this.SCHEDULES[i].validity.end) >= moment().endOf('isoWeek')) {
+            this.validSchedules.push(this.SCHEDULES[i]);
+          }
+        }
+        break;
+      case "monthly":
+        for (let i = this.SCHEDULES.length - 1; i >= 0; i--) {
+          if (moment(this.SCHEDULES[i].validity.start) <= moment().startOf('month') &&
+            moment(this.SCHEDULES[i].validity.end) >= moment().endOf('month')) {
+            this.validSchedules.push(this.SCHEDULES[i]);
+          }
+        }
+        break;
+      default:
+        for (let i = this.SCHEDULES.length - 1; i >= 0; i--) {
+          if (moment(this.SCHEDULES[i].validity.start) <= moment(this.today) &&
+            moment(this.SCHEDULES[i].validity.end) >= moment(this.today)) {
+            this.validSchedules.push(this.SCHEDULES[i]);
+          }
+        }
+        break;
+    }
+  }
+
+  getAppointments(date) {
+    return this.APPOINTMENTS.filter(appointment => {
+      let dateEval = appointment.date.format('DD/MM/YYYY') === date.format('DD/MM/YYYY');
+      let validScheduleEval = this.validSchedules.indexOf(appointment.schedule) >= 0;
+      let selectedScheduleEval = this.selectedSchedules.length > 0 ? this.selectedSchedules.indexOf(appointment.schedule) >= 0 : true;
+      return (dateEval && validScheduleEval && selectedScheduleEval);
+    });
   }
 
 }
